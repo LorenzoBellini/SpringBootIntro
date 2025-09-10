6add5a0ceae5e9f74043a553b5c63a99f5aa0ac7
lorenzobellini <lbellini226@gmail.com>
2025-09-10 14:45:21 -0300

    Subindo meu curso introdutório de Spring Boot

---
+++ springbootconfig/src/test/java/com/exerciciospringboot2/springbootconfig/SpringbootconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exerciciospringboot2.springbootconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
